7dfeb3d60a22026e418914f9fe8d321921f00d9f
Solumilken <s916526000@gmail.com>
2017-10-31 18:43:42 +0800

    not return empty string in a list

--- purewords_tokenizer.py
+++ purewords_tokenizer.py
@@ -4,4 +4,8 @@ import purewords
 class PureWordsTokenizer(object):
 
     def lcut(self, sentence):
-        return purewords.clean_sentence(sentence).split(' ')
+        clean_sentence = purewords.clean_sentence(sentence)
+        if clean_sentence == '':
+            return []
+        else:
+            return clean_sentence.split(' ')
