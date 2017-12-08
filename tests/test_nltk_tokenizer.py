61fb9684ada244f6edc31fa4a18d1f159402f77c
kelly19921107 <kelly19921107@gmail.com>
2017-12-08 12:39:45 +0800

    curse of trailing commas

--- tests/test_nltk_tokenizer.py
+++ tests/test_nltk_tokenizer.py
@@ -4,16 +4,17 @@ from .. import NltkTokenizer
 
 
 class test_NltkTokenizer(TestCase):
+
     def test_NltkTokenizer(self):
         tokenizer = NltkTokenizer()
         self.assertEqual(
             ['how', "'s", 'it', 'going', 'today', ',', 'mr.smith', '?', ],
-            tokenizer.lcut("How's it going today, Mr.Smith?", punct=False)
+            tokenizer.lcut("How's it going today, Mr.Smith?", punct=False),
         )
 
     def test_NltkTokenizer_punct(self):
         tokenizer = NltkTokenizer()
         self.assertEqual(
             ['how', "'", 's', 'it', 'going', 'today', ',', 'mr', '.', 'smith', '?', ],
-            tokenizer.lcut("How's it going today, Mr.Smith?", punct=True)
+            tokenizer.lcut("How's it going today, Mr.Smith?", punct=True),
         )
