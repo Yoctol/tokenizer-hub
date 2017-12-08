af9e5a52bc0776f2c060af205df2a08bd8af0b7c
kelly19921107 <kelly19921107@gmail.com>
2017-12-08 14:08:10 +0800

    added import statement

--- __init__.py
+++ __init__.py
@@ -3,3 +3,4 @@ from .jieba_tokenizer import JiebaTokenizer  # noqa
 from .jieba_tw_tokenizer import JiebaTWTokenizer  # noqa
 from .char_tokenizer import CharTokenizer  # noqa
 from .custom_jieba_tokenizer import CustomJiebaTokenizer  # noqa
+from .nltk_tokenizer import NltkTokenizer  # noqa
