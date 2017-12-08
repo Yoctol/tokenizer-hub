1f9645a5281a448673f260f907b1260f5047a39c
kelly19921107 <kelly19921107@gmail.com>
2017-12-08 12:31:36 +0800

    fixed flake8 issues

--- nltk_tokenizer.py
+++ nltk_tokenizer.py
@@ -1,14 +1,22 @@
 from nltk.tokenize import WordPunctTokenizer, word_tokenize
-    
 
-class NltkTokenizer(object):
-    
-    def __init__(self):
+from .base_tokenizer import BaseTokenizer
+
+
+class NltkTokenizer(BaseTokenizer):
+
+    def __init__(
+            self,
+        ):
         self.WPtokenizer = WordPunctTokenizer()
-        
-    def lcut(self, sentence, punct=True):
+
+    def lcut(
+            self,
+            sentence,
+            punct=True,
+        ):
         sentence = sentence.lower()
-        if punct == True:
+        if punct is True:
             return(self.WPtokenizer.tokenize(sentence))
         else:
             return(word_tokenize(sentence))
