8ac4d5ad371c8c3068368863296cda76d3dac43e
SoluMilken <s916526000@gmail.com>
2018-01-02 10:29:32 +0800

    added nltk_jieba_tokenizer (#510)

---
+++ nltk_jieba_tokenizer.py
@@ -0,0 +1,72 @@
+from typing import List
+import string
+
+import re
+import functools
+
+from .base_tokenizer import BaseTokenizer
+from .nltk_tokenizer import NltkTokenizer
+from .custom_jieba_tokenizer import CustomJiebaTokenizer
+
+
+class NltkJiebaTokenizer(BaseTokenizer):
+
+    def __init__(self):
+        self.nltk_tokenizer = NltkTokenizer()
+        self.custom_jieba_tokenizer = CustomJiebaTokenizer()
+        self.prog = re.compile('[{}]+'.format(string.printable))
+
+    def lcut(
+            self,
+            sentence: str,
+            punct: bool = True,
+            extra_words: List[str] = None,
+            cut_all: bool = False,
+            HMM: bool = True,
+            num_jobs: int = 4,
+        ) -> List[str]:
+        tokens = self.nltk_tokenizer.lcut(
+            sentence=sentence,
+            punct=punct,
+        )
+        output_tokens = []
+        for token in tokens:
+            eng_num = self.prog.sub('', token)
+            if len(eng_num) > 0:
+                output_tokens.append(
+                    self.custom_jieba_tokenizer.lcut(
+                        sentence=token,
+                        extra_words=extra_words,
+                        cut_all=cut_all,
+                        HMM=HMM,
+                    ),
+                )
+            else:
+                output_tokens.append([token])
+        return sum(output_tokens, [])
+
+    def lcut_sentences(
+            self,
+            sentences: List[str],
+            punct: bool = True,
+            extra_words: List[str] = None,
+            cut_all: bool = False,
+            HMM: bool = True,
+            num_jobs: int = None,
+        ) -> List[List[str]]:
+        from multiprocessing import cpu_count, Pool
+        if num_jobs is None:
+            num_jobs = cpu_count()
+        with Pool(num_jobs) as pool:
+            results = pool.map(
+                functools.partial(
+                    self.lcut,
+                    punct=punct,
+                    extra_words=extra_words,
+                    cut_all=cut_all,
+                    HMM=HMM,
+                    num_jobs=1,
+                ),
+                sentences,
+            )
+        return results
---
+++ tests/test_nltk_jieba_tokenizer.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+
+from .. import NltkJiebaTokenizer
+
+
+class NltkJiebaTokenizerTestCase(TestCase):
+
+    def setUp(self):
+        self.tokenizer = NltkJiebaTokenizer()
+
+    def test_lcut(self):
+        test_cases = [
+            ('我想喝珍珠奶茶半糖大杯',
+             ['我', '想', '喝', '珍珠奶茶', '半糖', '大杯']),
+            ('i would like to drink pearl milk tea.',
+             ['i', 'would', 'like', 'to', 'drink', 'pearl', 'milk', 'tea', '.']),
+            ('_ya_ 我想喝 _num_ 杯 _test_ 飲料 _end_',
+             ['_ya_', '我', '想', '喝', '_num_', '杯', '_test_', '飲料', '_end_']),
+            ('我想買12張(abc)股',
+             ['我', '想', '買', '12', '張', '(', 'abc', ')', '股']),
+            ('我想買 12 張 abc 股', ['我', '想', '買', '12', '張', 'abc', '股']),
+        ]
+        for test_case in test_cases:
+            with self.subTest(test_case=test_case):
+                self.assertEqual(
+                    test_case[1],
+                    self.tokenizer.lcut(sentence=test_case[0]),
+                )
