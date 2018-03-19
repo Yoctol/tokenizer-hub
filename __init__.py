5c2a82d1901641389b88f4e2844cfbf12e9a4b88
Solumilken <s916526000@gmail.com>
2018-03-19 18:17:46 +0800

    added num tokens to be reserved

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
+from itertools import permutations
+
 from .purewords_tokenizer import PureWordsTokenizer  # noqa
 from .parallel_jieba_tokenizer import ParallelJiebaTokenizer  # noqa
 from .chinese_char_tokenizer import (
@@ -7,3 +9,18 @@ from .chinese_char_tokenizer import (
 from .custom_jieba_tokenizer import CustomJiebaTokenizer  # noqa
 from .nltk_tokenizer import NltkTokenizer  # noqa
 from .nltk_custom_jieba_tokenizer import NltkCustomJiebaTokenizer  # noqa
+from .add_words import AddWords
+
+
+int_with_digits = ["_{}int_".format(num) for num in range(1, 13)]
+float_with_digits = [
+    "_{}float{}_".format(pair[0], pair[1]) for pair in list(
+        permutations(list(range(1, 13)), 2)
+    ) if pair[1] < 5
+]
+
+RESERVED_TOKENS = [
+    "_int_",
+    "_float_",
+    "_num_",
+] + int_with_digits + float_with_digits
