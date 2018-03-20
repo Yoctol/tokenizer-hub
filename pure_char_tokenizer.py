fbdb9b1236fdc1547296d3b5fec39a84cfd6ab9e
Solumilken <s916526000@gmail.com>
2018-03-20 17:50:26 +0800

    rename chinese char tokenizer to char tokenizer for purewords

--- pure_char_tokenizer.py
+++ pure_char_tokenizer.py
@@ -2,36 +2,41 @@ from typing import List
 import functools
 import re
 
+import purewords
+import string
+
 from .base_tokenizer import BaseTokenizer
-from nlu.tokenizers import RESERVED_TOKENS
 
 
-class ChineseCharTokenizer(BaseTokenizer):
+class CharTokenizerForPureWords(BaseTokenizer):
 
     def __init__(self):
-        reg_pattern = RESERVED_TOKENS + \
-            ["[0-9]+\.[0-9]+"] + \
-            ["[a-zA-Z]+"] + \
-            ["[0-9]+"]
-
         self.prog = re.compile(
-            r"{}".format("|".join(reg_pattern)),
+            "[{}]+".format(re.escape(string.printable)),
         )
 
     def lcut(
             self,
             sentence: str,
             **kwargs  # noqa
-        ) -> List[str]:
+        ):
         not_chinese_elements = self.prog.findall(sentence)
         pure_chinese_sentence = self.prog.sub("X", sentence)
         tokens = list(pure_chinese_sentence)
         index = 0
         for ti, token in enumerate(tokens):
-            if token == "X":
+            if token == 'X':
                 tokens[ti] = not_chinese_elements[index]
                 index += 1
         return tokens
+
+    def cut(
+            self,
+            sentence: str,
+        ) -> str:
+        result = self.lcut(sentence)
+        for char in result:
+            yield char
 
     def lcut_sentences(
             self,
@@ -49,3 +54,25 @@ class ChineseCharTokenizer(BaseTokenizer):
                 sentences,
             )
         return results
+
+
+class PureChineseCharTokenizer(BaseTokenizer):
+
+    def __init__(self):
+        self.purechartokenizer = purewords.PureWords(
+            tokenizer=CharTokenizerForPureWords(),
+        )
+
+    def lcut(
+            self,
+            sentence: str,
+        ) -> List[str]:
+        return self.purechartokenizer.clean_sentence(sentence).split(" ")
+
+    def cut(
+            self,
+            sentence: str,
+        ) -> str:
+        result = self.lcut(sentence=sentence)
+        for char in result:
+            yield char
